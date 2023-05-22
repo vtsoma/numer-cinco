3bf6fb26263754631a71eabc8f7211ed968dfd30
vtsoma <cheesetkh39@gmail.com>
2023-05-22 11:39:33 +0300

    Maybe a solution?

---
+++ fibonacci/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> nums = new List<int> { 1, 1 };
+
+for (int i = 0; i < 40; i++)
+{
+    int numberOne = nums[nums.Count - 1];
+    int numberTwo = nums[nums.Count - 2];
+
+    nums.Add(numberOne + numberTwo);
+
+    Console.WriteLine(nums[i]);
+}
